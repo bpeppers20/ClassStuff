be4484572969567dd8fa8a3a008c7a01f0691594
Bryant Peppers <bryantpeppers@gmail.com>
2020-03-11 17:28:44 -0500

    Second Commit: Semaphore ready

--- bin_adder.c
+++ bin_adder.c
@@ -5,12 +5,28 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-
+#include <semaphore.h>
 
 int main (int argc, char* argv[])
 {
+	
+	//Make a child process within the child process in order for adding
+	//Semaphores and another fork Proccess here
+	//This will also be the only file editing share memory
+	int * nums = 0;
+	key_t key = ftok("proj3", 10);
+	int shmid = shmget(key, 1024, 0666|IPC_CREAT);
+	if (shmid < 0)
+	{
+		perror("Failed to connect to shared memory");
+		exit(EXIT_FAILURE);
+	}
+	nums = (int*) shmat(shmid, (void*)0, 0); // attach shm
+	nums[0] = 1;
 	while(1)
 	{
 		printf("In Child\n");
+		nums [0] = nums[0] + 1;
+		printf("%d\n",nums[0]);
 	}
 }
--- master.c
+++ master.c
@@ -11,13 +11,16 @@
 pid_t id = -1; // global process id
 char *file = "bin_adder.txt";// File
 // Shared Memory possible global
+int * shmPtr = 0; // Initilze for security reasons
+int shm_id = 0; 
 
 //Signal Handling
 void kill_child(int sig) // Handles SIGKILL with SIGALRM
 {
-//	shmdt(shmPtr); // detach shared memory
-//	shmctl(id, IPC_RMID,NULL); // Remove shared memory
-	printf("Process %d Automatically killed\n", id); // log error message
+	printf("Value in Shared Memory: %d\n",shmPtr[0]);
+	shmdt(shmPtr); // detach shared memory
+	shmctl(id, IPC_RMID,NULL); // Remove shared memory
+	printf("Process Automatically killed\n"); // log error message
 	// File operations
 	kill(id,SIGKILL);
 
@@ -25,36 +28,43 @@ void kill_child(int sig) // Handles SIGKILL with SIGALRM
 
 void sigIntHandle (int sig) // handles SIGINT
 {
-//	shmdt(shmPtr); // detach shared memory
-//	shmctl(id,IPC_RMID,NULL); // remove shared memory
+	printf("Value in Shared Memory: %d\n",shmPtr[0]);
+	shmdt(shmPtr); // detach shared memory
+	shmctl(id,IPC_RMID,NULL); // remove shared memory
 	//FILE operation
-	printf("\nProcess %d manually killed\n",id);
+	printf("\nProcess manually killed\n");
 	kill(id,SIGKILL); // kill process
-
 }
 
 int main (int argc, char* argv[])
 {
+	// This file will create shared memory and initlize it, but 
+	// will not edit. Will only read
 	id = getpid();// get parent process
 	if (id < 0)
 	{
 		perror(" Failed to receive parent id\n");
+		return -1;
 	}
 	int e; // For execing
 	signal(SIGALRM, (void(*)(int))kill_child); // capture SIGALARM
 	signal(SIGINT,(void(*)(int))sigIntHandle); // capture SIGINT
 	// Shared Memory Operations
-	//
-	//
-	//
-	//
+	key_t key = ftok("proj3",10); // create accessable key
+	int shm_id = shmget(key,1024, 0666|IPC_CREAT); // get shared memory id
+	if (shm_id < 0)
+	{
+		perror("Failed to connect to shared memory"); // check if connected
+		return -1;
+	}
+	shmPtr = (int*) shmat(shm_id, (void*)0, 0); // attach shared memory
 	// Shared Memory Operations 
 	
 	//set Alarm
 	id = fork(); // Create Child Process
 	if (id > 0)
 	{
-		alarm(2); // set alarm in parent
+		alarm(100); // set alarm in parent
 	}
 	if (id < 0) // terminate on failure
 	{
@@ -67,7 +77,9 @@ int main (int argc, char* argv[])
 	}
 	int status;
 	wait(&status);
-
+	shmdt(shmPtr); // detach shm
+	shmctl(shm_id,IPC_RMID,NULL);
+	
 
 
 
